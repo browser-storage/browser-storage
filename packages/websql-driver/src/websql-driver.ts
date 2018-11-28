373ca0aec25d29f6920e373d62755704b985df53
Igor Katsuba <ikatsuba@swiftcom.uk>
2018-11-28 13:56:16 +0300

    fix WebsqlDriver#getItem

--- packages/websql-driver/src/websql-driver.ts
+++ packages/websql-driver/src/websql-driver.ts
@@ -43,9 +43,9 @@ export class WebsqlDriver implements Driver {
 
   public async getItem<T>(key: string): Promise<T> {
     const result = await this._executeSql(`SELECT * FROM ${this.options.storeName} WHERE key = ? LIMIT 1`, [key]);
-    return result.rows.length && this._serializer.deserialize<T>(
-      result.rows.item(0).value
-    );
+    return result.rows.length
+      ? this._serializer.deserialize<T>(result.rows.item(0).value)
+      : null;
   }
 
   public async hasItem(key: string): Promise<boolean> {
